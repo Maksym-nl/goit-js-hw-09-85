dc61531f70837e02e61331489455c2882affefd8
Maksym-nl <119403118+Maksym-nl@users.noreply.github.com>
2023-07-13 09:42:26 +0000

    Deploying to gh-pages from @ Maksym-nl/goit-js-hw-09-85@22b87d6eb95c2ef39128ef2a4be125a12a45dd4b 🚀

---
+++ 01-color-switcher.71626750.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;function o(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){n=setInterval(o,1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.71626750.js.map
